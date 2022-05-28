cb29fb0c1cac09225bac1fb881dce853c9d2441c
Ezra1337 <gleb97972@mail.ru>
2022-05-28 12:13:39 +0600

    dz2

---
+++ ves/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double n = 10.17;
+        double x = Math.round(n);
+        System.out.println("result = " + x);
+
+    }
+}
